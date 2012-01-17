12a826b0e6a6ac8b8530f77a0a9aa34eb3a8c459
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-01-17 11:26:49 -0600

    add makefile and build.sh. bump version

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "adding tzinfo..."
+echo "(function(){window.TZINFO=" > tz.js
+cat tz.json >> tz.js
+echo "})();" >> tz.js
+echo "adding dst..."
+curl https://raw.github.com/chrisdickinson/dst.js/master/index.js 2>/dev/null >> tz.js
+echo "adding tz..."
+cat index.js >> tz.js
+COMMAND=
+for i in uglifyjs jsmin NULL; do
+	command -v $i 2>&1>/dev/null && COMMAND=$i && break
+done 
+
+command -v $COMMAND 2>&1>/dev/null && cat tz.js | $COMMAND > tz.min.js
+
+echo "done."
